2757e3e801670e97d9798d136ccdd06f6f91b965
thinhdht <dohongthinhdht1999@gmail.com>
2019-07-03 23:51:32 -0400

    Conventional base-routing

--- Vidly/Vidly/App_Start/RouteConfig.cs
+++ Vidly/Vidly/App_Start/RouteConfig.cs
@@ -12,7 +12,14 @@ namespace Vidly
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            //Define from specific to generic route
+            //Define a custom route
+            routes.MapRoute(
+                "MoviesByReleaseDate",//name of the route
+                "movies/released/{year}/{month}",//url pattern
+                new { controller ="Movies",action ="ByReleaseDate" },//specify the defaults
+                //add constrain to url
+                new { year =@"\d{4}",month =@"\d{2}"}); // @"\" = "\\" 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movie/Random
+        public ActionResult Random() //ActionResult helps to return various action results
+        {
+            var movie = new Movies() { Name = "Bumblebee!" };
+
+            return View(movie);
+            //return Content("Hello World!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return new ViewResult();
+            // return RedirectToAction(name of action, controller, pass argument to target action);
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        //movies
+        // ? is for unnullable
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
